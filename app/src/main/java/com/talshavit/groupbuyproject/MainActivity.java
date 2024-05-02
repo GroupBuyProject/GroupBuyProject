f53186c2788511eccb989d798f8fb0aa49a9880a
talshavit <talshavit84@gmail.com>
2024-05-02 11:27:16 +0300

    test

--- app/src/main/java/com/talshavit/groupbuyproject/MainActivity.java
+++ app/src/main/java/com/talshavit/groupbuyproject/MainActivity.java
@@ -12,6 +12,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Log.d("lala", "lala");
+        Log.d("lala", "lalallllll");
     }
 }
