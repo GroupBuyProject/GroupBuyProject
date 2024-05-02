51d31bb4a670a6a8d348e1917b1cd7b976831558
talshavit <talshavit84@gmail.com>
2024-05-02 13:48:27 +0300

    MeowBottomNavigation

--- app/src/main/java/com/talshavit/groupbuyproject/MainActivity.java
+++ app/src/main/java/com/talshavit/groupbuyproject/MainActivity.java
@@ -5,13 +5,47 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import com.etebarian.meowbottomnavigation.MeowBottomNavigation;
+
+
 public class MainActivity extends AppCompatActivity {
+
+    private MeowBottomNavigation bottomNavigation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Log.d("lala", "lalallllll");
+        bottomNavigation = findViewById(R.id.bottomNavigation);
+
+        bottomNavigation.add(new MeowBottomNavigation.Model(1, R.drawable.home));
+        bottomNavigation.add(new MeowBottomNavigation.Model(2, R.drawable.home));
+        bottomNavigation.add(new MeowBottomNavigation.Model(3, R.drawable.home));
+
+        bottomNavigation.setOnClickMenuListener(new MeowBottomNavigation.ClickListener() {
+            @Override
+            public void onClickItem(MeowBottomNavigation.Model item) {
+                switch (item.getId()){
+                    case 1:
+                        Log.d("lala", "1");
+                        break;
+                    case 2:
+                        Log.d("lala", "2");
+                        break;
+                    case 3:
+                        Log.d("lala", "3");
+                        break;
+                }
+            }
+        });
+
+        bottomNavigation.setOnShowListener(new MeowBottomNavigation.ShowListener() {
+            @Override
+            public void onShowItem(MeowBottomNavigation.Model item) {
+            }
+        });
+
+
     }
 }
